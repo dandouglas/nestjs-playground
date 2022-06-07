0d6522f62d9a9e050a22217e3e72d5861ffb4129
Daniel Douglas <dan.douglas.01@gmail.com>
2022-06-07 21:13:39 +0100

    task(auth-api): auth and users logic

--- apps/auth-api/src/app.module.ts
+++ apps/auth-api/src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService, UsersService],
+  providers: [AppService],
 })
 export class AppModule {}
