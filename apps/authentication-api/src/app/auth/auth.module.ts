96487d891217c16d59cde9b1ad7cc07be015d0dc
Daniel Douglas <dan.douglas.01@gmail.com>
2022-06-08 21:55:01 +0100

    task(auth): generate jwt

--- apps/authentication-api/src/app/auth/auth.module.ts
+++ apps/authentication-api/src/app/auth/auth.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   providers: [AuthService, LocalStrategy],
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' }
+    })
+  ],
+  exports: [AuthService]
 })
 export class AuthModule {}
